7f59a06f9718775d9ef6515089a159babf5325f1
Olena Sydorova <olena.sydorova@gmail.com>
2023-05-18 21:57:23 +0200

    update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// player.setCurrentTime(
+//   JSON.parse(localStorage.getItem('videoplayer-current-time'))
+// );
+
+// player.on(
+//   'timeupdate',
+//   throttle(data => {
+//     localStorage.setItem(
+//       'videoplayer-current-time',
+//       JSON.stringify(data.seconds)
+//     );
+//   }, 1000)
+// );
