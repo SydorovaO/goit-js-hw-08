6181290d5d520ae1274e198f285b2302392b6d6e
Olena Sydorova <olena.sydorova@gmail.com>
2023-05-18 20:16:51 +0200

    update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,7 @@
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+
 const refs = {
   form: document.querySelector('.feedback-form'),
   email: document.querySelector('input'),
@@ -5,28 +9,30 @@ const refs = {
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.form.addEventListener('input', onFormInput);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
 populateForm();
 
 function onFormSubmit(e) {
   e.preventDefault();
+
   e.currentTarget.reset();
-  localStorage.removeItem('feedback-form-state');
+  localStorage.removeItem(STORAGE_KEY);
 }
 
 function onFormInput() {
   const email = refs.email.value;
+  console.log(email);
   const message = refs.message.value;
   const formData = {
     email,
     message,
   };
   const formDataToString = JSON.stringify(formData);
-  localStorage.setItem('feedback-form-state', formDataToString);
+  localStorage.setItem(STORAGE_KEY, formDataToString);
 }
 
 function populateForm() {
-  const savedObject = localStorage.getItem('feedback-form-state');
+  const savedObject = localStorage.getItem(STORAGE_KEY);
   const parsedObject = JSON.parse(savedObject);
 
   if (parsedObject) {
