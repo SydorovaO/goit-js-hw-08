74115e89652096d5e20b9e77e8a5401b48cfdb1e
Olena Sydorova <olena.sydorova@gmail.com>
2023-05-20 11:55:41 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -13,25 +13,23 @@ form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onFormInput, 500));
 populateForm();
 
-function createFormData(email, message) {
-  return {
-    email: email,
-    message: message,
+function onFormInput() {
+  const formData = {
+    email: email.value,
+    message: message.value,
   };
+  const formDataToString = JSON.stringify(formData);
+  localStorage.setItem(STORAGE_KEY, formDataToString);
 }
 
 function onFormSubmit(e) {
   e.preventDefault();
-  const formData = createFormData(email.value, message.value);
-  console.log(formData);
+  console.log({
+    email: email.value,
+    message: message.value,
+  });
   e.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
-}
-
-function onFormInput() {
-  const formData = createFormData(email.value, message.value);
-  const formDataToString = JSON.stringify(formData);
-  localStorage.setItem(STORAGE_KEY, formDataToString);
 }
 
 function populateForm() {
@@ -39,45 +37,7 @@ function populateForm() {
   const parsedObject = JSON.parse(savedObject);
 
   if (parsedObject) {
-    refs.email.value = parsedObject.email;
-    refs.message.value = parsedObject.message;
+    email.value = parsedObject.email;
+    message.value = parsedObject.message;
   }
 }
-
-// const formData = function (email, message) {
-//   return {
-//     email: email,
-//     message: message,
-//   };
-// };
-
-// function onFormSubmit(e) {
-//   e.preventDefault();
-
-//   formData(refs.email.value, refs.message.value);
-//   console.log(formData);
-
-//   e.currentTarget.reset();
-//   localStorage.removeItem(STORAGE_KEY);
-// }
-// function onFormInput(e) {
-//   formData = createFormData(refs.email.value, refs.message.value);
-//   saveFormDataToLocalStorage();
-// }
-
-// function updateFormData() {}
-
-// function saveFormDataToLocalStorage() {
-//   const formDataToString = JSON.stringify(formData);
-//   localStorage.setItem(STORAGE_KEY, formDataToString);
-// }
-
-// function populateForm() {
-//   const savedObject = localStorage.getItem(STORAGE_KEY);
-//   const parsedObject = JSON.parse(savedObject);
-
-//   if (parsedObject) {
-//     email.value = parsedObject.email;
-//     message.value = parsedObject.message;
-//   }
-// }
